cb6e3cd3b45f0ee6b7749fff4456f65deb511760
dmonster5150-droid <dmonster5150@gmail.com>
2025-10-19 11:55:43 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,23 @@
-export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHLombrAwRIJXwG11vgTq8X7EdqFInBUA",
+  authDomain: "clikfix-418b4.firebaseapp.com",
+  projectId: "clikfix-418b4",
+  storageBucket: "clikfix-418b4.appspot.com",
+  messagingSenderId: "557392194925",
+  appId: "1:557392194925:web:a99893be3ba48e3a1d7e64",
+  measurementId: "G-NDF4W0ZBT4"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, provider, db, storage };
