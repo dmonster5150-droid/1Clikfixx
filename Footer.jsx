497c0d6a20050955ac629ecac342aeac92381c80
dmonster5150-droid <dmonster5150@gmail.com>
2025-10-21 14:28:51 -0700

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,2 +1,10 @@
-import React from 'react'
+import React from "react";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>© {new Date().getFullYear()} 1ClikFix — All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer; React from 'react'
 export default function Footer(){ return (<footer className='footer'>© 2025 1ClikFixx</footer>) }
