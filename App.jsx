2ee8ca77fe955c617d7b36638f9e95cc9b11237f
dmonster5150-droid <dmonster5150@gmail.com>
2025-10-23 17:10:37 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -7,7 +7,8 @@ import ProviderPortal from "./pages/ProviderPortal";
 import Login from "./pages/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-
+import { testFirebaseConnection } from "./firebaseTest";
+testFirebaseConnection();
 function App() {
   return (
     <BrowserRouter>
